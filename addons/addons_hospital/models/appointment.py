585f21e1ed4f2e1fb5cd2e2ae12bebc2bc3170a7
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-11-04 08:21:51 +0100

    add: statusbar + button action

--- addons/addons_hospital/models/appointment.py
+++ addons/addons_hospital/models/appointment.py
@@ -11,10 +11,29 @@ class HospitalAppointment(models.Model):
     patient_id = fields.Many2one("hospital.patient", string="Patient", required=True, tracking=True )
     date_appointment = fields.Date(string="Date", tracking=True)
     note = fields.Text(string="Note", tracking=True)
+    state = fields.Selection([
+        ('draft','Draft'),('confirmed','Confirmed'),('ongoing','Ongoing'),
+        ('done','Done'),('cancel','Cancelled')], default='draft', tracking=True)
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
             if vals.get('reference') == 'New' or vals.get('reference') is None:
                 vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
-        return super().create(vals_list) 
+        return super().create(vals_list)
+
+    def action_confirm(self):
+        for rec in self:
+            rec.state = 'confirmed'
+
+    def action_ongoing(self):
+        for rec in self:
+            rec.state = 'ongoing'
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
